43feab89b8fb76ac33ad9c04996f702fb49ca832
deepali <deepaalipatil@gmail.com>
2022-01-23 23:24:01 +0530

    ADDED: Test case for retrive data from database

---
+++ src/test/java/com/bridgelabz/Employee_Payroll/EmployeePayrollTest.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.Employee_Payroll;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.Employee_Payroll.EmployeePayrollService.IOService;
+
+public class EmployeePayrollTest {
+	
+	 @Test
+		public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries()
+		{
+			EmployeePayroll[] arrayOfEmployees = {
+					new EmployeePayroll(1, "Ratan Tata", 100000.0, LocalDate.now()),
+					new EmployeePayroll(2, "Bill Gates", 200000.0, LocalDate.now()),
+					new EmployeePayroll(3, "Elon Musk", 300000.0, LocalDate.now())
+			};
+			EmployeePayrollService employeePayrollService;
+			employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+			employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+			
+			employeePayrollService.printData(IOService.FILE_IO);
+			long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+			Assert.assertEquals(3, entries);
+			
+		}
+		@Test
+		public void givenFile_WhenRead_ShouldReturnNumberOfEntries() {
+			EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+			long entries = employeePayrollService.readDataFromFile(IOService.FILE_IO);
+			Assert.assertEquals(3, entries);
+		}
+		@Test
+		public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount(){
+			
+			EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+			List<EmployeePayroll> employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+			System.out.println(employeePayrollData.size());
+			Assert.assertEquals(4, employeePayrollData.size());
+		}
+		
+}
